834c0e3673042ddb35b5914e999f3c47eda78ce5
Ramakrishnan Palaninathan <igramki@gmail.com>
2024-11-10 23:31:38 -0500

    Securing the API

--- infra/api.ts
+++ infra/api.ts
@@ -1,15 +1,19 @@
 import { table } from "./storage";
 
 // Create the API
+// Create the API
 export const api = new sst.aws.ApiGatewayV2("Api", {
   transform: {
     route: {
       handler: {
         link: [table],
       },
+      args: {
+        auth: { iam: true }
+      },
     }
   }
 });
 
-api.route("POST /notes", "packages/functions/src/create.main");
-api.route("GET /notes/{id}", "packages/functions/src/get.main");
+api.route("POST /notes", { handler: "packages/functions/src/create.main", dev: false});
+api.route("GET /notes/{id}", { handler: "packages/functions/src/get.main", dev: false});
